38405792163abd73d4d198e3c9501bea80fcfbf1
geoffrey woodcock <theatomproject@gmail.com>
2018-04-30 16:45:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+class CashRegister
+  attr_accessor :total, :discount 
+ 
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @cart = []
+  end
+   
+  def add_item(item, price, quantity = 1)
+    item_specs = {}
+    item_specs[:name_of_item] = item
+    item_specs[:price] = price
+    item_specs[:quantity] = quantity
+    @cart << item_specs
+    @total += price * quantity
+  end
+  
+  def apply_discount
+    if @discount == 0 
+      "There is no discount to apply."
+    else
+      @total -= @total * @discount / 100
+      "After the discount, the total comes to $#{@total}."
+    end  
+  end
+  
+  def item
+     
+  end
+end
+
+ 
 
