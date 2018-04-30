631f65b16298f5bcd3e76dae6bbf08a624089fe0
geoffrey woodcock <theatomproject@gmail.com>
2018-04-30 17:54:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,9 +21,6 @@ class CashRegister
     end
   end
 
-
-
-  
   def apply_discount
     if @discount == 0 
       "There is no discount to apply."
@@ -39,49 +36,8 @@ class CashRegister
 
     def items
       @all_items
+  end
  end
- end
 
 
-
-# class CashRegister
-#   attr_accessor :total, :discount, :price, :items
-
-#   def initialize(discount = 0)
-#     @total = 0
-#     @discount = discount
-#     @items = []
-#   end
-
-#   def add_item(item, price, quantity = 1)
-#     @price = price
-#     @total += price * quantity
-#     if quantity > 1
-#       counter = 0
-#       while counter < quantity
-#         @items << item
-#         counter += 1
-#       end
-#     else
-#       @items << item
-#     end
-#   end
-# end
-
-  # def apply_discount
-  #   if @discount > 0
-  #     @to_take_off = (price * discount)/100
-  #     @total -= @to_take_off
-  #     return "After the discount, the total comes to $#{total}."
-  #   else
-  #     return "There is no discount to apply."
-  #   end
-  # end
-
-#   def void_last_transaction
-#     @total -= @price
-#   end
-
-# end
- 
 
