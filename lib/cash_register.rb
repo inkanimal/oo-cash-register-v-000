a62436d1eccb7192d17df1f86129984b18d90f43
geoffrey woodcock <theatomproject@gmail.com>
2018-04-30 17:43:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -61,15 +61,15 @@ class CashRegister
     end
   end
 
-  def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else
-      return "There is no discount to apply."
-    end
-  end
+  # def apply_discount
+  #   if @discount > 0
+  #     @to_take_off = (price * discount)/100
+  #     @total -= @to_take_off
+  #     return "After the discount, the total comes to $#{total}."
+  #   else
+  #     return "There is no discount to apply."
+  #   end
+  # end
 
   def void_last_transaction
     @total -= @price
