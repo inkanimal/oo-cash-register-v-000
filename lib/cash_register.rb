a97039ab4bba5b400aa604966d76c71c3f8685ba
geoffrey woodcock <theatomproject@gmail.com>
2018-04-30 17:46:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -45,19 +45,19 @@ class CashRegister
 #     @items = []
 #   end
 
-  # def add_item(item, price, quantity = 1)
-  #   @price = price
-  #   @total += price * quantity
-  #   if quantity > 1
-  #     counter = 0
-  #     while counter < quantity
-  #       @items << item
-  #       counter += 1
-  #     end
-  #   else
-  #     @items << item
-  #   end
-  # end
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
 
   # def apply_discount
   #   if @discount > 0
