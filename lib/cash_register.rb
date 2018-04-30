3bf8eabde81d7510abfef435c20dc67a612a2f18
geoffrey woodcock <theatomproject@gmail.com>
2018-04-30 17:22:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,80 +1,80 @@
-# class CashRegister
-#   attr_accessor :total, :discount, :price
- 
-#   def initialize(discount = 0)
-#     @total = 0 
-#     @discount = discount
-#     @all_items = []
-#   end
-   
-#   def add_item(item, price, quantity = 1)
-#     item_specs = {}
-#     item_specs[:name_of_item] = item
-#     item_specs[:price] = price
-#     item_specs[:quantity] = quantity
-#     @all_items << item_specs
-#     @total += price * quantity
-#   end
-  
-#   def apply_discount
-#     if @discount == 0 
-#       "There is no discount to apply."
-#     else
-#       @total -= @total * @discount / 100
-#       "After the discount, the total comes to $#{@total}."
-#     end  
-#   end
-  
-#   def items
-#     list = []
-#     @all_items.each do |item|
-#       item.flatten
-#     end
-#   end
-  
-#   def void_last_transaction
-#     @total -= @price
-#   end
-  
-# end
-
 class CashRegister
-  attr_accessor :total, :discount, :price, :items
-
+  attr_accessor :total, :discount, :price
+ 
   def initialize(discount = 0)
-    @total = 0
+    @total = 0 
     @discount = discount
-    @items = []
+    @all_items = []
   end
-
+   
   def add_item(item, price, quantity = 1)
-    @price = price
+    item_specs = {}
+    item_specs[:name_of_item] = item
+    item_specs[:price] = price
+    item_specs[:quantity] = quantity
+    @all_items << item_specs
     @total += price * quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
-    end
   end
-
+  
   def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
+    if @discount == 0 
+      "There is no discount to apply."
     else
-      return "There is no discount to apply."
+      @total -= @total * @discount / 100
+      "After the discount, the total comes to $#{@total}."
+    end  
+  end
+  
+  def items
+    list = []
+    @all_items.each do |item|
+      item.flatten
     end
   end
-
+  
   def void_last_transaction
     @total -= @price
   end
-
+  
 end
+
+# class CashRegister
+#   attr_accessor :total, :discount, :price, :items
+
+#   def initialize(discount = 0)
+#     @total = 0
+#     @discount = discount
+#     @items = []
+#   end
+
+#   def add_item(item, price, quantity = 1)
+#     @price = price
+#     @total += price * quantity
+#     if quantity > 1
+#       counter = 0
+#       while counter < quantity
+#         @items << item
+#         counter += 1
+#       end
+#     else
+#       @items << item
+#     end
+#   end
+
+#   def apply_discount
+#     if @discount > 0
+#       @to_take_off = (price * discount)/100
+#       @total -= @to_take_off
+#       return "After the discount, the total comes to $#{total}."
+#     else
+#       return "There is no discount to apply."
+#     end
+#   end
+
+#   def void_last_transaction
+#     @total -= @price
+#   end
+
+# end
  
 
